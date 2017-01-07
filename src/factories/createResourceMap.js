deab1721afac98fc56f56c62a00e61e65c7fa0d1
Gajus Kuizinas <gajus@gajus.com>
2017-01-07 12:24:45 +0200

    docs: document what is a request in the context of a manifest

--- src/factories/createResourceMap.js
+++ src/factories/createResourceMap.js
@@ -2,6 +2,11 @@
 
 /**
  * Creates an object that maps request to module ID.
+ *
+ * A 'request' is a require path used in the application
+ * resolved to a relative path to the project context, e.g.
+ * if `./src/test/index.js` requires `require('./style.css')`
+ * the the 'request' is `./src/test.style.css`.
  */
 export default (manifest: Object): Object => {
   const map = {};
